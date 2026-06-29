7e3ddd9e3cc0bef916eb0680c32cba8727a5d82e
Aishmanya <aishmanyanishi@gmail.com>
2026-06-29 20:03:08 +0530

    Project 4 completed

--- DOM/Basic_Projects/GuessTheNo/chaiaurcode.js
+++ DOM/Basic_Projects/GuessTheNo/chaiaurcode.js
@@ -1,4 +1,6 @@
-const random = parseInt((Math.random() * 100 )+1)
+
+
+let random = parseInt((Math.random() * 100 )+1)
 // console.log(random)
 
 const userInput = document.querySelector('#guessField')  //.value
@@ -7,6 +9,8 @@ const prevGuesses = document.querySelector('.guesses')
 const Remaining = document.querySelector('.lastResult')
 const lowOrHi = document.querySelector('.lowOrHi')
 const resultbox = document.querySelector('.resultParas')
+
+const p = document.createElement('p')
 
 let guessNo = 1
 let playGame = true
@@ -43,6 +47,7 @@ function validateGuess(userGuess){
             endGame()
         }
         else{
+            displayGuess(userGuess)
             checkGuess(userGuess)
         }
     }
@@ -55,23 +60,54 @@ function checkGuess(userGuess){
         displayMsg('Yay! You WON !!!!')
         endGame()
     }
+    else if (userGuess > random) {displayMsg('your no. is too high')}
+
+    else if (userGuess < random) {displayMsg('your no. is too low')}
+
 }
 
 function displayGuess(userGuess){
-    // to display
-    
+    // to update userGuess no like empty that to take next guess then update prevArr remaining attempts etc
+
+    // userInput.innerHTML=''               wrong
+    userInput.value='';
+    guessNo++;
+    Remaining.innerHTML=`${11 - guessNo}`;
+    prevGuesses.innerHTML += `${userGuess} ,`;
 }
 
 function displayMsg(msg){
-    // 
+    // to display any msg
+    lowOrHi.innerHTML=`<h2> ${msg} </h2>`
 }
 
 function endGame(){
-    // 
+    // to clear the values and disable the input value of userInput 
+    userInput.value='';
+    userInput.setAttribute('disabled','') ;//always goes in pair so main hame disable karna tha toh toh aisehe as a pair empty quote dal diye
+    p.classList.add('button');
+    p.innerHTML=`<h2 id='newgame'>Start New Game</h2>`;
+    resultbox.appendChild(p);
+    playGame = false;
+    newGame();
+
 }
 
 function newGame(){
-    // 
+    // resets all vars and start again
+    const newGamebtn= document.querySelector('#newgame');
+    newGamebtn.addEventListener('click', function(e){
+        random = parseInt((Math.random() * 100 )+1);
+        prevArr=[];
+        guessNo=1;
+        prevGuesses.innerHTML='';
+        Remaining.innerHTML=`${11 - guessNo}`;
+        userInput.removeAttribute('disabled');
+        resultbox.removeChild(p);
+
+        playGame=true;
+    });
+
 }
 
 
