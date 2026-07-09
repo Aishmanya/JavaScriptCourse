d2b184b5ac70f710a345d50e8b9fc6ba145a39ab
Aishmanya <aishmanyanishi@gmail.com>
2026-07-09 11:45:43 +0530

    OOP Started

---
+++ OOP/oop.js
@@ -0,0 +1,3 @@
+// ~~~~~~~~~~~~~~~~~~~~~~~~ Javascript is a prototype-based language ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// means that objects inherit properties and methods directly from other objects, instead of inheriting from classes (as in Java or C++).
